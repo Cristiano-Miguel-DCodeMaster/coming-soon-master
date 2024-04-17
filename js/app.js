b1a71ce23bcfa12a5e90db3fbd27c3c3a83afaf3
Cristiano-Miguel-DCodeMaster <cristianomiguel128@gmail.com>
2024-04-17 12:08:24 +0100

    Js Almost finished

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.querySelector('.button')
+    .addEventListener('submit', (event) => {
+        event.preventDefault();
+        const email = document.getElementById('email').value;
+        const alertsms = document.querySelector('.alertsms0');
+        const error = document.querySelector('.error0');
+        if (email == []) {
+            alertsms.classList.remove('alertsms0');
+            alertsms.classList.add('alertsms');
+            alertsms.innerHTML = "Please, type your email.";
+            error.classList.remove('error0');
+            error.classList.add('error');
+        } else if (email != []) {
+            this.submit();
+        }
+    });
