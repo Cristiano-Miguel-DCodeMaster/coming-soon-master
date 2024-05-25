9dba00bc2489abe45da2dc4cf19f48aa02358016
Cristiano-Miguel-DCodeMaster <cristianomiguel128@gmail.com>
2024-05-25 16:14:14 +0100

    The Js is working perfectly now!

--- js/app.js
+++ js/app.js
@@ -4,13 +4,11 @@ document.getElementById('button')
         const email = document.getElementById('email').value;
         const alertsms = document.querySelector('.alertsms0');
         const error = document.querySelector('.error0');
-        if (email == []) {
+          if (!email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
             alertsms.classList.remove('alertsms0');
             alertsms.classList.add('alertsms');
-            alertsms.innerHTML = "Please, type your email.";
             error.classList.remove('error0');
             error.classList.add('error');
-        } else if (email != []) {
-            this.submit();
+            alertsms.innerHTML = "Please provide a valid email";
         }
     });
